be4a38cfb5cd08fe6c54520bb00fcf2bfd414ae8
webdevyoo <yjk7454@naver.com>
2024-09-10 00:05:19 +0900

    feat: `app.js` - startGame() 함수 추가 및 3초 카운팅 기능 구현

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 new Vue({
   el: '#app',
   data: {
-    myChoice: null
+    myChoice: null,
+    count: 3
+  },
+  methods: {
+    startGame: function () {
+      if (this.myChoice == null) {
+        alert('가위 바위 보 중 하나를 선택해주세요') 
+        return;
+      }
+      
+      let countDown = setInterval(() => {
+        this.count --;
+        if (this.count === 0) clearInterval(countDown); // Interval을 멈추라는 의미 (countDown은 interval 식별값)
+      }, 1000)
+    }
   }
 })
