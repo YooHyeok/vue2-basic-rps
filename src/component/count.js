e5e2055959fe51ce420065fe8913d3c1e9555ffb
webdevyoo <yjk7454@naver.com>
2024-09-22 02:58:55 +0900

    feat: `count.js` - CDN 방식 Count 컴포넌트 구현

---
+++ src/component/count.js
@@ -0,0 +1,13 @@
+Vue.component('Count', {
+  template: html`
+    <div class="small-2 columns text-center">
+      <h1 style="font-size:100px;">
+        <strong>{{ count }}</strong>
+      </h1>
+    </div>
+  `,
+	name: "Count",
+	props: {
+    count: null,
+	}
+});
