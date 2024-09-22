924ad7953d65919594d117af97ff0e500075372c
webdevyoo <yjk7454@naver.com>
2024-09-23 00:22:15 +0900

    feat: `radio.js` - CDN 방식 Radio 컴포넌트 구현

---
+++ src/component/radio.js
@@ -0,0 +1,23 @@
+Vue.component('Radio', {
+  template: html`
+    <div class="row">
+      <div class="small-8 small-offset-2 columns text-center">
+        <label 
+          v-for="(rps,index) in selects"
+          class="radio-label"
+        >
+          <input 
+            type="radio" 
+            v-model="choice.user" 
+            :value="rps.value"
+          > {{rps.name}}
+        </label>
+      </div>
+    </div>
+  `,
+	name: "Radio",
+	props: {
+		selects: null,
+		choice: null,
+	}
+});
