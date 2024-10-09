0aa97a556810a83c1ab9152e331e5e8cc5d8323d
webdevyoo <yjk7454@naver.com>
2024-10-09 22:39:18 +0900

    fix: `main.js` - vue.js 이름 변경 및 상위 디렉토리로 이동, Root컴포넌트 참조 주소 수정

---
+++ main.js
@@ -0,0 +1,4 @@
+import Root from "./src/component/root.js"
+new Vue({
+  render: (h) => { return h(Root) }
+}).$mount('#app')
--- src/vue.js
+++
@@ -1,4 +0,0 @@
-import Root from "./component/root.js"
-new Vue({
-  render: (h) => {return h(Root)}
-}).$mount('#app')
